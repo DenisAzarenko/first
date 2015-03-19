712cec15efed67939dc8cd9229e12e7ed366edf0
dazarenko <dazarenko@navionics.com>
2015-03-19 13:17:15 +0300

    initialize

---
+++ first/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  first
+//
+//  Created by azarenkoda on 3/19/15.
+//  Copyright (c) 2015 ox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
